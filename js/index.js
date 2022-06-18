ceebd3abf438a17db2ed939b5597d7f112d7b51d
nidhikumari <kumari9936nidhi@gmail.com>
2022-06-18 15:55:22 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
-//Create function to select elements
-// const selectElement = (element) => document.querySelector(element);
-
-// //Open and close nav on click
-
-// selectElement(element, '.menu-icons'), addEventListener(type, 'click', listner, () => {
-//     selectElement(element, 'nav').classList.toggle(token, 'active');
-// })
-
-
-// const bars = document.querySelector('.menu-icons');
-// const navBar = document.querySelector('.nav-list');
-// bars.addEventListener('click', () => {
-//     navBar.classList.toggle('active');
-//     bars.classList.toggle('rotate');
-// })
+const selectElement = (element) => document.querySelector(element);
+selectElement('.menu-icons').addEventListener('click', () => {
+    selectElement('nav').classList.toggle('active');
+});
